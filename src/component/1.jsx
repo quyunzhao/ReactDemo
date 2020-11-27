e37c13626943d188574ebb79e36c574d30e6eeb2
jingqu <1264352733@qq.com>
2020-11-27 21:08:27 +0800

    绑定文本框的值

--- src/component/1.jsx
+++ src/component/1.jsx
@@ -1,10 +1,30 @@
 import React, { Component } from "react";
 
 export default class One extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      num: 20,
+    };
+  }
+  handlerChange(e) {
+    const value = e.target.value;
+    console.log(value);
+    this.setState({ num: value }, function () {
+      console.log(this.state.num);
+    });
+  }
+
   render() {
     return (
       <div>
         <h1> 这里是h1标题</h1>
+        <p>{this.state.num}</p>
+        <input
+          type="text"
+          value={this.state.num}
+          onChange={this.handlerChange.bind(this)}
+        />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+// import App from "./App";
+import App from "./component/1";
 
 ReactDOM.render(
   <React.StrictMode>
