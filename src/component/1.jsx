fc643b1b09566722b8708087b4fb6a21e55f6bc2
jingqu <1264352733@qq.com>
2020-11-27 20:54:16 +0800

    one

---
+++ src/component/1.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class One extends Component {
+  render() {
+    return (
+      <div>
+        <h1> 这里是h1标题</h1>
+      </div>
+    );
+  }
+}
