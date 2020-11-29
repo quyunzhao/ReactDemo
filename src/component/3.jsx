bcc226e4f1a6d8523184f05a54056c1db7ab8d5a
jingqu <1264352733@qq.com>
2020-11-29 12:07:28 +0800

    this.props的使用（父传子）

--- src/component/3.jsx
+++ src/component/3.jsx
@@ -3,9 +3,12 @@ import React, { Component } from "react";
 
 // 子组件
 class Header extends React.Component {
+  // 给 props 添加默认值
   static defaultProps = {
     bgc: "skyblue",
+    title: "默认标题",
   };
+
   render() {
     return (
       <div>
@@ -19,6 +22,8 @@ class Header extends React.Component {
         >
           {/* this.props 要获取调用这个组件的标签属性 */}
           {this.props.title}
+          {/* 特殊写法 专门获取 子组件被调用的时候， 写成双标签时，标签中的字元素 */}
+          {this.props.children}
         </header>
       </div>
     );
@@ -37,8 +42,9 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Header title={this.state.title} bgc="#cff"></Header>
-        <Header title="列表标题"></Header>
+        <Header title={this.state.title} bgc="#cff" />
+        <Header title="列表标题" />
+        <Header>子元素</Header>
       </div>
     );
   }
