e88982f737642b6673db2934fc7689c4a64c94a3
jingqu <1264352733@qq.com>
2020-11-29 21:31:13 +0800

    key 值得使用

--- src/component/4.jsx
+++ src/component/4.jsx
@@ -4,15 +4,52 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      list: [11, 25, 45, 47, 89],
+      list: [
+        { id: 111, value: "第一" },
+        { id: 112, value: "第二" },
+        { id: 113, value: "第三" },
+        { id: 114, value: "第四" },
+        { id: 115, value: "第五" },
+      ],
     };
   }
+
+  // 方法一
+  handleClick() {
+    this.setState({
+      list: [
+        { id: 111, value: "第一" },
+        { id: 112, value: "第二" },
+        { id: 113, value: "第三" },
+        { id: 114, value: "第四" },
+        { id: 115, value: "第五" },
+      ],
+    });
+  }
+
+  // 方法二
+  handleClick2 = () => {
+    this.setState({
+      list: [
+        { id: 116, value: "第六" },
+        { id: 111, value: "第一" },
+        { id: 112, value: "第二" },
+        { id: 113, value: "第三" },
+        { id: 114, value: "第四" },
+        { id: 115, value: "第五" },
+      ],
+    });
+  };
+
   render() {
     return (
       <div>
+        <button onClick={this.handleClick.bind(this)}>重置</button>
+        <button onClick={() => this.handleClick2()}>添加</button>
+
         <ul>
           {this.state.list.map((v, k) => {
-            return <li key={k}>{v}</li>;
+            return <li key={v.id}>{v.value}</li>;
           })}
         </ul>
       </div>
