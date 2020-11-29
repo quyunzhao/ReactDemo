fc95b6e69607847cd134a24913c120fc5f54a630
jingqu <1264352733@qq.com>
2020-11-29 22:00:25 +0800

    双向绑定箭头函数

--- src/component/1.jsx
+++ src/component/1.jsx
@@ -5,6 +5,7 @@ export default class One extends Component {
     super();
     this.state = {
       num: 20,
+      num2: 220,
     };
   }
   handlerChange(ee) {
@@ -15,15 +16,33 @@ export default class One extends Component {
     });
   }
 
+  handlerChange2 = (e) => {
+    const value = e.target.value;
+    // console.log(value);
+    this.setState({ num2: value }, function () {
+      console.log(this.state.num2);
+    });
+  };
+
   render() {
     return (
       <div>
         <h1> 这里是h1标题</h1>
+        <hr />
+
         <p>{this.state.num}</p>
         <input
           type="text"
           value={this.state.num}
           onChange={this.handlerChange.bind(this)}
+        />
+        <hr />
+
+        <p>{this.state.num2}</p>
+        <input
+          type="text"
+          value={this.state.num2}
+          onChange={(e) => this.handlerChange2(e)}
         />
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 // import App from "./App";
-// import App from "./component/1";
+import App from "./component/1";
 // import App from "./component/2";
-import App from "./component/4";
+// import App from "./component/3";
+// import App from "./component/4";
 
 ReactDOM.render(
   <React.StrictMode>
