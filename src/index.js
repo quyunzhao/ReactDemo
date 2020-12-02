22489d0b7a84811df9f40e009b9a57f168351a65
jingqu <1264352733@qq.com>
2020-12-02 19:57:10 +0800

    使用context进行props 属性值的多级传递

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ import "./index.css";
 // import App from "./component/3";
 // import App from "./component/4";
 // import App from "./component/5";
-import App from "./component/6";
+// import App from "./component/6";
+import App from "./component/7";
 
 ReactDOM.render(
   <React.StrictMode>
