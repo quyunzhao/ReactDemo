cac323e635da2d2fae2c0f70b5965b17587105dd
jingqu <1264352733@qq.com>
2020-12-06 19:58:54 +0800

    其他路由组件

--- src/reactRouter/App.jsx
+++ src/reactRouter/App.jsx
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 // 引入路由模块
-import { Router, Route, hashHistory } from "react-router";
+import {
+  Router,
+  Route,
+  hashHistory,
+  IndexRoute,
+  IndexRedirect,
+  Link,
+} from "react-router";
 
 // 引入对应的组件
 import Home from "./Home";
@@ -13,7 +20,7 @@ export default class App extends Component {
     return (
       <div>
         <ul>
-          <li>
+          {/* <li>
             <a href="#/app/home">首页</a>
           </li>
           <li>
@@ -21,6 +28,16 @@ export default class App extends Component {
           </li>
           <li>
             <a href="#/app/detail/1345">详情</a>
+          </li> */}
+
+          <li>
+            <Link to="/app/home">首页</Link>
+          </li>
+          <li>
+            <Link to="/app/list">列表</Link>
+          </li>
+          <li>
+            <Link to="/app/detail/1345">详情</Link>
           </li>
         </ul>
         <hr />
@@ -34,6 +51,12 @@ export default class App extends Component {
 let routes = (
   <Router history={hashHistory}>
     <Route path="/app" component={App}>
+      {/* IndexRoute 在保持 /app 路径不变的情况下，设置默认的展示页面 */}
+      {/* <IndexRoute component={Home}></IndexRoute> */}
+
+      {/* IndexRedirect 在访问 /app 会直接重定向到  /app/home 所对应的视图*/}
+      <IndexRedirect to="home"></IndexRedirect>
+
       {/* 相对路径的写法 */}
       <Route path="home" component={Home} />
       {/* 绝对路径的写法 */}
