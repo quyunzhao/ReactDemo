005eb39a7718b1f0b9ea9bcc6aad3f4395fc02fc
jingqu <1264352733@qq.com>
2022-03-19 20:25:54 +0800

    函数作为子元素

--- src/component/Test/index.jsx
+++ src/component/Test/index.jsx
@@ -3,6 +3,16 @@ import React, { useRef } from "react";
 export default function Test() {
   const inputDom = useRef();
 
+  // 调用子元素回调 numTimes 次，来重复生成组件
+  function Repeat(props) {
+    const items = [];
+
+    for (let i = 0; i < props.numTimes; i++) {
+      items.push(props.children(i, 5));
+    }
+    return <div>{items}</div>;
+  }
+
   return (
     <div>
       <input
@@ -20,6 +30,13 @@ export default function Test() {
           inputDom.current.focus();
         }}
       />
+      <Repeat numTimes={5}>
+        {(index, w) => (
+          <div key={index}>
+            This is item {index} {w}in the list
+          </div>
+        )}
+      </Repeat>
     </div>
   );
 }
